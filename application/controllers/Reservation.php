668ae367086f36feb869b13c99874a9da0c6e547
Unknown <aboniaa@gmail.com>
2019-07-28 22:07:29 +0200

    bypass cors issues

--- application/controllers/Reservation.php
+++ application/controllers/Reservation.php
@@ -1,4 +1,8 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 use Swagger\Annotations as SWG;
 /**
  * @package
--- application/models/MyModel.php
+++ application/models/MyModel.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept,Client-Service,Auth-Key');
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MyModel extends CI_Model {
